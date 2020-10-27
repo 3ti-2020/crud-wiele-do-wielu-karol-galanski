27663da3f6adc9c987686b055d2cf10d45c6d9c5
Galanskyy <42960637+Galanskyy@users.noreply.github.com>
2020-10-27 12:02:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cont">
+        <div class="top"></div>
+        <div class="left"></div>
+        <div class="main">
+            <?php
+                $serwername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "lib";
+
+                $conn = new mysqli($serwername, $username, $password, $dbname);
+
+                $result = $conn->query("SELECT id_autor_tytul, name, tytul FROM autor_tytul, autor, tytul WHERE autor_tytul.id_autor=autor.id_autor AND autor_tytul.id_tytul=tytul.id_tytul");
+
+                echo("<table border=1>");
+                echo("
+                <th>id</th>
+                <th>name</th>
+                <th>tytul</th>
+                ");
+
+                while($row = $result->fetch_assoc() ){
+                    echo("<tr>");
+                    echo("<td>".$row['id_autor_tytul']."</td>");
+                    echo("<td>".$row['name']."</td>");
+                    echo("<td>".$row['tytul']."</td>");
+                    echo("</tr>");
+                }  
+                echo("</table>");
+
+                
+
+
+            ?>
+        </div>
+        <div class="ft"></div>
+    </div>
+</body>
+</html>
