9b4dc8dbb1a4c0b014ed33bbac3c52b6fe93505d
Galanskyy <42960637+Galanskyy@users.noreply.github.com>
2020-11-04 11:05:27 +0100

    Add files via upload

--- plik1.php
+++ plik1.php
@@ -9,41 +9,32 @@
 <body>
     <div class="cont">
         <div class="top">
-
             <?php
 
                 session_start();
 
-                if( isset($_POST['haslo']) && $_POST['haslo'] =="zaq1@WSX"){
-                    $_SESSION['logowanie'] = 1;
-                }
-
                 if( isset($_GET['akcja']) && $_GET['akcja'] == "wyloguj" ){
                     unset($_SESSION['logowanie']);
                 }
-                if( isset($_SESSION['logowanie']) && $_SESSION['logowanie'] == 1){
-                    ?>
-                    <div class="login">
-                    <h1 class='zalogowany'>ZALOGOWANY</h1>
-                    <a href='index.php?akcja=wyloguj'>WYLOGUJ</a>
-                    </div>
-                    <?php
+
+                if( !isset($_SESSION['logowanie']) ){
+                ?>  
+
+                    <form action="index.php" method="post" class="logowanie">
+                        <input type="text" name="login" placeholder="podaj login">
+                        <input type="text" name="haslo" placeholder="podaj haslo">
+                        <input type="submit" value="Zaloguj">
+                    </form>
+                <?php
                 }else{
-                    ?>
-                    <div class="login">
-                    <h1 class='wylogowany'>NIE ZALOGOWANY</h1>
-                    </div>
-                    
-                    <?php
+                    echo("<h1 class='zalogowany'>ZALOGOWANY</h1>");
+                    echo("<a href='plik1.php?akcja=wyloguj'>WYLOGUJ</a>");
                 }
 
-
-
-                
             ?>
 
             <div class="linki">
-                <a href="plik1.php" class="link"><i class="fas fa-sign-in-alt" id="icon"></i>LOGIN</a>
+                <a href="index.php" class="link"><i class="fas fa-home" id="icon"></i>MAIN</a>
                 <a href="../card-main/index.html" class="link"><i class="fas fa-clipboard" id="icon"></i>KARTA</a>
                 <a href="https://github.com/3ti-2020/crud-wiele-do-wielu-karol-galanski" class="link"><i class="fab fa-github" id="icon"></i>GITHUB</a>
             </div>
