4524c71f9c6bab396e0a5b7a32e3eb0c971ec052
Galanskyy <42960637+Galanskyy@users.noreply.github.com>
2020-12-10 14:57:26 +0100

    Add files via upload

--- blog/index.php
+++ blog/index.php
@@ -10,6 +10,7 @@
     <div class="cont">
         <div class="head">
             <h1 class='blog_title'>GAMES BLOG</h1>
+            
         </div>
         <div class="main">
             <?php
@@ -24,16 +25,17 @@
                 while($row1 = $result1->fetch_assoc() ){
                     echo("<article class='post'>");
                     echo("<h2 class='tytul'>".$row1['tytul']."</h2>");
+                    echo("<img src='".$row1['zdj']."' class='zdj'>");
                     echo("<p class='tresc'>");
                     echo($row1['tresc']);
                     echo("</p>");
                     echo("<h3 class='tag'>");
-                        $result2 = $conn->query("SELECT * FROM blog_tag JOIN blog_post_tag ON blog_tag.id_tag=blog_post_tag.id_tag WHERE blog_post_tag.id_post");
+                        $result2 = $conn->query("SELECT * FROM blog_tag JOIN blog_post_tag ON blog_tag.id_tag=blog_post_tag.id_tag WHERE blog_post_tag.id_post=".$row1['id_post']);
                         while($row2 = $result2->fetch_assoc()){
                             echo("#".$row2['tag']);
                         }
                     echo("</h3>");
-                    
+                    echo("</article>");
                 }
 
 
