4a9fd602decef30d12e7dbff1af2092fbf286b83
Galanskyy <42960637+Galanskyy@users.noreply.github.com>
2020-12-15 13:22:05 +0100

    Add files via upload

--- blog/szukanie_tagu.php
+++ blog/szukanie_tagu.php
@@ -9,7 +9,7 @@
 <body>
     <div class="cont">
         <div class="head">
-            <h1 class='blog_title'>GAMES BLOG</h1>
+            <h1><a href="index.php" class='blog_title'>GAMES BLOG</a></h1>
             
         </div>
         <div class="main">
@@ -21,7 +21,7 @@
 
                 $conn = new mysqli($servername, $username, $password, $dbname);
 
-                $result1 = $conn->query("SELECT * FROM blog_post");
+                $result1 = $conn->query("SELECT DISTINCT * FROM blog_post JOIN blog_post_tag ON blog_post.id_post=blog_post_tag.id_post JOIN blog_tag ON blog_post_tag.id_tag=blog_tag.id_tag WHERE blog_tag.tag = '".$_POST['szukaj_tag']."'");
                 while($row1 = $result1->fetch_assoc() ){
                     echo("<article class='post'>");
                     echo("<h2 class='tytul'>".$row1['tytul']."</h2>");
@@ -38,6 +38,7 @@
                                 #<input type='submit' class='in_tag' name='szukaj_tag' value='".$row2['tag']."'>
                                 </form>");
                         }
+                        
                     echo("</h3>");
                     echo("</div>");
                     echo("</article>");
